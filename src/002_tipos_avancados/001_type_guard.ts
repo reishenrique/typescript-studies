1e8edf85ee919eee93242755e887b0088d040f50
Henrique Reis <contatohenriquereis@gmail.com>
2024-03-11 17:07:46 -0300

    feature: type guard keyof

--- src/002_tipos_avancados/001_type_guard.ts
+++ src/002_tipos_avancados/001_type_guard.ts
@@ -47,3 +47,30 @@ const example2: any = 'Hello'
 if (typeof example2 === 'string') {
   console.log('É uma string:', example.toUpperCase())
 }
+
+// 3. keyof
+
+// Definindo um tipo para representar um objeto com várias propriedades
+type Car = {
+  brand: string
+  model: string
+  year: number
+}
+
+// Função que recebe um objeto e uma chave e retorna o valor da chave se ela existir no objeto
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] | undefined {
+  return obj[key]
+}
+
+// Objeto de exemplo
+const car: Car = {
+  brand: 'Toyota',
+  model: 'Corolla',
+  year: 2000,
+}
+
+// Acessando propriedades do objeto de forma dinâmica
+const brand: string | undefined = getProperty(car, 'brand') // Saída 'Toyota'
+const model: string | undefined = getProperty(car, 'model') // Saída: 'Corolla'
+const year: number | undefined = getProperty(car, 'year') // Saída: 2020
+const color: string | undefined = getProperty(car, 'color') // Saída: undefined, porque 'color' não é uma propriedade de Car
